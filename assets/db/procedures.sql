716d7d1b0072418563bab1ea90ef78e545ea3e01
Jason Melendez <jason.melendez@gmail.com>
2014-03-21 16:34:25 -0700

    Adding SQL to source control

---
+++ assets/db/procedures.sql
@@ -0,0 +1,61 @@
+-- --------------------------------------------------------------------------------
+-- Generate Token
+-- Generates a device auth token for a user
+-- --------------------------------------------------------------------------------
+DELIMITER $$
+
+CREATE PROCEDURE `generateToken` (IN usernameIn VARCHAR(24), IN passIn VARCHAR(24), IN deviceSig VARCHAR(64), IN deviceDesc VARCHAR(64))
+BEGIN
+	/* get user ID */
+	SET @userId = (SELECT `id` FROM users WHERE `username` = usernameIn AND `pass` = passIn);
+        SET @msg = NULL;
+	
+	IF @userId IS NOT NULL THEN
+		/* see if this device exists */
+		SET @deviceId = (SELECT `id` FROM sync_device WHERE `user_id` = @userId AND `signature` = deviceSig);
+		IF @deviceId IS NOT NULL THEN
+			/* get the existing token */
+			SET @token = (SELECT `token` FROM sync_device WHERE `id` = @deviceId);
+
+			/* if token is empty, meaning it has been previously expired or cleared, generate a new one */
+			if @token < 1 THEN
+				SET @token = CAST(MD5(CONCAT(CONCAT('SafeText-hashsalt', usernameIn), UNIX_TIMESTAMP())) AS CHAR);
+				UPDATE sync_device SET `token`=@token, `token_expires`=DATE_ADD(CURDATE(),INTERVAL 5 DAY), `description`=deviceDesc WHERE `user_id` = @userId AND `id` = @deviceId; 
+			ELSE
+				/* reset the init flag */
+				UPDATE sync_device SET `is_initialized`=0, `description`=deviceDesc WHERE `user_id` = @userId AND `id` = @deviceId;
+				/* clear the sync queue */
+				DELETE FROM sync_queue WHERE `user_id` = @userId AND `device_id` = @deviceId;
+			END IF;
+			
+		ELSE
+			/* make sure that there aren’t too many devices already registered */
+			SET @numDevices = (SELECT COUNT(*) FROM `sync_device` WHERE user_id=@userId);
+
+			IF @numDevices < 2 THEN
+				/* create new device entry */
+				SET @tokenString = CAST(MD5(CONCAT(CONCAT('SafeText-hashsalt', usernameIn), UNIX_TIMESTAMP())) AS CHAR);
+				INSERT INTO sync_device (id,user_id,signature,description,is_initialized,token,token_expires) VALUES('', @userId, deviceSig, deviceDesc, '0', @tokenString, DATE_ADD(CURDATE(),INTERVAL 5 DAY));
+				IF LAST_INSERT_ID() IS NOT NULL THEN
+					SET @token = @tokenString;
+				ELSE
+					SET @userId = 0;
+					SET @token = NULL;
+					SET @msg = "Unable to create new token";
+				END IF;
+			ELSE
+				SET @userId = 0;
+				SET @msg = "Too many devices registered. Unregister unused devices using the web client Settings page";
+			END IF;
+		END IF;
+
+	ELSE
+		/* user/pass didn't match */
+		SET @userId = 0;
+        SET @token = NULL;
+		SET @msg = "No match found for that username and password";
+    END IF;
+
+	SELECT @userId AS id, @token AS token, @msg AS msg;
+
+END
---
+++ assets/db/tables.sql
@@ -0,0 +1,144 @@
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `user_id` int(10) unsigned NOT NULL,
+  `contact_user_id` int(10) unsigned NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `phone` varchar(32) NOT NULL,
+  `is_whitelist` tinyint(1) unsigned NOT NULL,
+  `is_blocked` tinyint(1) unsigned NOT NULL,
+  KEY `user_id` (`user_id`,`contact_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `is_read` tinyint(1) unsigned NOT NULL,
+  `is_important` tinyint(1) unsigned NOT NULL,
+  `is_draft` tinyint(1) unsigned NOT NULL,
+  `sent_date` datetime NOT NULL,
+  `read_date` datetime NOT NULL,
+  `expire_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participants`
+--
+
+DROP TABLE IF EXISTS `participants`;
+CREATE TABLE IF NOT EXISTS `participants` (
+  `message_id` int(10) unsigned NOT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  `is_sender` tinyint(1) unsigned NOT NULL,
+  KEY `message_id` (`message_id`,`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+CREATE TABLE IF NOT EXISTS `payments` (
+  `merch_payment_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `amount` double(4,2) unsigned NOT NULL,
+  `approval_code` varchar(16) NOT NULL,
+  `payment_date` datetime NOT NULL,
+  PRIMARY KEY (`merch_payment_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription_levels`
+--
+
+DROP TABLE IF EXISTS `subscription_levels`;
+CREATE TABLE IF NOT EXISTS `subscription_levels` (
+  `id` mediumint(8) unsigned NOT NULL,
+  `name` varchar(16) NOT NULL,
+  `description` varchar(64) NOT NULL,
+  `cpm` decimal(4,2) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sync_device`
+--
+
+DROP TABLE IF EXISTS `sync_device`;
+CREATE TABLE IF NOT EXISTS `sync_device` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `signature` varchar(64) NOT NULL,
+  `label` varchar(32) NOT NULL,
+  `description` varchar(32) NOT NULL,
+  `is_initialized` tinyint(1) unsigned NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `token_expires` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `token` (`token`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sync_queue`
+--
+
+DROP TABLE IF EXISTS `sync_queue`;
+CREATE TABLE IF NOT EXISTS `sync_queue` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` tinyint(3) unsigned NOT NULL,
+  `device_id` tinyint(3) unsigned NOT NULL,
+  `date_added` datetime NOT NULL,
+  `tablename` varchar(12) NOT NULL,
+  `pk` int(10) unsigned NOT NULL,
+  `vals` text NOT NULL,
+  `is_pulled` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `phone` varchar(32) NOT NULL,
+  `pass` varchar(16) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_last_pass_update` datetime NOT NULL,
+  `language` varchar(2) NOT NULL,
+  `notifications_on` tinyint(1) unsigned NOT NULL,
+  `whitelist_only` tinyint(1) unsigned NOT NULL,
+  `enable_panic` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
