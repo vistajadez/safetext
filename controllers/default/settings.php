f7422c69d7efc2737a40571129c17ca007735b0b
Jason Melendez <jason.melendez@gmail.com>
2014-04-29 20:46:05 -0700

    Enable settings and devices management on Settings page. Also updated site nav.

---
+++ controllers/default/settings.php
@@ -0,0 +1,48 @@
+<?php
+require_once ( MS_PATH_BASE . DS . 'lib' . DS . 'safetext' . DS . 'clientcontroller.php' );
+
+/**
+ * Web Client Controller.
+ *
+ *
+ */
+class SettingsController extends SafetextClientController {
+
+	/**
+	 * Default Action.
+	 * 
+	 * Called when no action is defined. Will display settings summary/edit form.
+	 *
+	 * @param MsView $viewObject
+	 * @return void
+	 */
+	 public function defaultAction(&$viewObject) {
+		// forward to the web client page if logged in, otherwise to the login page
+		if (!$this->init($viewObject)) {
+			$this->forward($viewObject, 'login', 'auth');
+		} else {
+			// load stats for all contacts and messages for current user
+			$folderStats = current($this->db->call("folderStats('" . $this->user->getValue('id') . "')"));
+			
+			// load all registered devices
+			$devicesArray = $this->db->call("devices('" . $this->user->getValue('id') . "')");
+			$devices = new SafetextModelCollection('SafetextDevice', $this->config, $this->db);
+			$devices->load($devicesArray);
+			
+			//title
+			$viewObject->setTitle('Settings');
+			
+			// set view data
+			$viewObject->setValue('folderStats', $folderStats);
+			$viewObject->setValue('devices', $devices);
+		
+		
+		
+		
+		}
+	 }
+	
+	
+	
+	
+}
